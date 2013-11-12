3cfaa810720e9e47aa78079ddcb19fd384cead9e
rodrigomaia17 <rodrigo.maia.pereira@gmail.com>
2013-11-12 03:14:00 -0200

    initial commit

---
+++ FakeBookAdsApi.Tests/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using FakeBookAdsApi.Controllers;
+
+namespace FakeBookAdsApi.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConsigoLerUmAdUser()
+        {
+            var usersController = new UsersController();
+            long accountId = 0;
+            string accessToken = "";
+            var response = usersController.Read(accountId, accessToken);
+
+            var jsonResponse = JsonConvert.SerializeObject(response);
+
+            AreEqualIgnoreWhiteSpace(readUserResponse, jsonResponse);
+
+        }
+
+        public void AreEqualIgnoreWhiteSpace(string expected, string actual)
+        {
+            string stringOne = expected;
+            string stringTwo = actual;
+
+            string fixedStringOne = Regex.Replace(stringOne, @"\s+", String.Empty);
+            string fixedStringTwo = Regex.Replace(stringTwo, @"\s+", String.Empty);
+
+            Assert.AreEqual(fixedStringOne,fixedStringTwo,false);
+            
+        }
+
+        private const string readUserResponse = @"{ ""data"": [{
+                ""id"": 121211,
+                ""permissions"": [
+                 1,
+                                    2,
+                                    3,
+                                    4,
+                                    5,
+                                    7
+                                  ],
+                                  ""role"": 1001
+                                }
+                              ]
+                            }";
+    }
+}
---
+++ FakeBookAdsApi/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FakeBookAdsApi.Controllers
+{
+    public class UsersController
+    {
+        [Route("act_{account_id:long}/users")]
+        public ReadResponse Read(long account_id,string accessToken)
+        {
+            return new ReadResponse();
+        }
+    }
+
+    public class ReadResponse
+    {
+        public DataReadResponse[] data;
+
+        public ReadResponse()
+        {
+            data = new DataReadResponse[]
+                {
+                    new DataReadResponse()
+                        {
+                            id = 121211,
+                            permissions = new int[] {1, 2, 3, 4, 5, 7},
+                            role = 1001
+                        }
+                };
+        }
+    }
+
+    public class DataReadResponse
+    {
+        public long id { get; set; }
+        public int[] permissions { get; set; }
+        public int role { get; set; }
+    }
+}
